4084c152231af716937b51f54541fa3cc4557496
ole <muklek99@gmail.com>
2024-02-26 14:56:45 +0700

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,6 @@
     <link rel="stylesheet" href="{{ asset('cropimage-master/cropimage.css') }}" />
 
     <link rel="stylesheet" href="{{asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}} ">
-
     <script src=" {{asset('plugins/jquery/jquery.min.js')}}"></script>
     <!-- Bootstrap -->
     <script src="{{asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
@@ -46,8 +45,6 @@
     <script src="{{asset('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
     <script src="{{asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
     <script src="{{asset('plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
-
-
     {{-- <script src="{{ asset('js/jquery.min.js') }}"></script> --}}
     <script src="{{ asset('js/settings.js') }}"></script>
     <script src="{{ asset('select2/js/select2.full.min.js') }}"></script>
