cfe0c7c52d160e56a9451b34655379d3b4043185
oles <muklek99@gmail.com>
2024-06-05 13:54:24 +0700

    mutasi

--- app/Http/Controllers/MutationController.php
+++ app/Http/Controllers/MutationController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Mutation;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+use Illuminate\Support\Facades\DB;
+use Intervention\Image\Facades\Image;
+
 
 class MutationController extends Controller
 {
@@ -14,106 +18,149 @@ class MutationController extends Controller
 
     public function dataMutasi(Request $req)
     {
-        $data = DB::table("mutasi")->where('status','=','aktif');
+        $data = DB::table("mutasi")
+        ->join('siswa', 'mutasi.id_mutasi', '=', 'siswa.id_siswa')
+        ->join('kelas', 'mutasi.id_mutasi', '=', 'kelas.id_kelas')
+        ->join('program_keahlian', 'mutasi.id_mutasi', '=', 'program_keahlian.id_program_keahlian')
+        ->select('mutasi.id_mutasi as idM', 'mutasi.tglMutasi as tglMts', 'mutasi.noSuratMutasi as NSM', 'mutasi.pindah as PN',
+        'mutasi.AlasanPindah as AP', 'mutasi.suratPernyataan as SP', 'siswa.namaSiswa as NS', 'kelas.kelas as KLS');
         $datatable =  DataTables::of($data);
         return $datatable
             ->addIndexColumn()
             ->addColumn('action', function ($row) {
                 $bt = '
             <div class="btn-group" role="group" aria-label="Basic example">
-                <button id="' . $row->id_tahun_ajaran .'" class="edit btn btn-info btn-xs" type="button">edit</button>
-                <button id="' . $row->id_tahun_ajaran .'" class="hapus btn btn-warning btn-xs" type="button">hapus</button>
+                <button id="' . $row->idM .'" class="edit btn btn-info btn-xs" type="button">edit</button>
             </div>
                 
                 ';
                 return $bt . "";
             })
-            ->addColumn('tahunAjaran', function ($row) {
-                $a = $row->tahun_ajaran;
+            ->addColumn('nama', function ($row) {
+                $a = $row->NS;
                 return $a . "";
             })
-            ->addColumn('keterangan', function ($row) {
-                $a = $row->keterangan;
-                return $a . "";
+            ->addColumn('kelas', function ($row) {
+                $b = $row->KLS;
+                return $b . "";
             })
-            ->addColumn('status', function ($row) {
-                $a = '
-                <span>'.  $row->status.' </span>
-                ';
-                return $a . "";
+            ->addColumn('tgl', function ($row) {
+                $c = $row->tglMts;
+                return $c . "";
             })
-            ->rawColumns(['action', 'tahunAjaran','keterangan','status'])
+            ->addColumn('pindah', function ($row) {
+                $d = $row->PN;
+                return $d . "";
+            })
+            ->addColumn('alasanPindah', function ($row) {
+                $e = $row->AP;
+                return $e . "";
+            })
+            ->rawColumns(['action', 'nama','kelas','tgl','pindah','alasanPindah'])
             ->make(true);
     }
 
-    public function tambah_mutasi()
+    public function updateMutasi($id)
+    {
+        return view('mutasi.update_mutasi', compact("id"));
+    }
+
+    public function addMutasi($id)
     {
         return view('mutasi.add_mutasi', compact("id"));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function uploadBerkasMutasi(Request $request) {
+        $data = array();
+
+        function acakangkahuruf($panjang)
+        {
+            $karakter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            $string = "";
+            for ($i = 0; $i <= $panjang; $i++) {
+                $pos = rand(0, strlen($karakter) - 1);
+                $string .= $karakter[$pos];
+            }
+            return $string;
+        }
+
+        $fileName = rand(00000, 99999) . acakangkahuruf(1) . ".jpg";
+
+        if ($request->file('suratPernyataan') != "") {
+            $surat_pernyataan = Image::make($request->file("suratPernyataan"));
+            $surat_pernyataan->resize(500, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            if (file_exists($request->suratPernyataanLama)) {
+                unlink($request->suratPernyataanLama);
+            }
+            $surat_pernyataan->save("../gambar/siswa/suratPernyataan/surat_pernyataan" . $fileName, 100);
+            $data["suratPernyataan"] = "gambar/siswa/suratPernyataan/surat_pernyataan" . $fileName;
+        } else {
+            $data["suratPernyataan"] = $request->suratPernyataanLama;
+        }
+
+        if ($request->file('berkasVideo') != "") {
+            $berkas_video = Image::make($request->file("berkasVideo"));
+            $berkas_video->resize(500, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            if (file_exists($request->berkasVideoLama)) {
+                unlink($request->berkasVideoLama);
+            }
+            $berkas_video->save("../gambar/siswa/berkasVideo/berkas_video" . $fileName, 100);
+            $data["berkasVideo"] = "gambar/siswa/berkasVideo/berkas_video" . $fileName;
+        } else {
+            $data["berkasVideo"] = $request->berkasVideoLama;
+        }
+
+        if ($request->file('suratMutasi') != "") {
+            $surat_mutasi = Image::make($request->file("suratMutasi"));
+            $surat_mutasi->resize(500, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            if (file_exists($request->suratMutasiLama)) {
+                unlink($request->suratMutasiLama);
+            }
+            $surat_mutasi->save("../gambar/siswa/suratMutasi/surat_mutasi" . $fileName, 100);
+            $data["suratMutasi"] = "gambar/siswa/suratMutasi/surat_mutasi" . $fileName;
+        } else {
+            $data["suratMutasi"] = $request->suratMutasiLama;
+        }
+
+        $update = DB::table("mutasi")
+            ->where("id_mutasi", $request->id)
+            ->update($data);
+
+        if ($update == 0 || $update == 1) {
+            return 1;
+        } else {
+            return "k";
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Mutation  $mutation
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Mutation $mutation)
+    public function simpanMutasi(Request $req)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Mutation  $mutation
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Mutation $mutation)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Mutation  $mutation
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Mutation $mutation)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Mutation  $mutation
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Mutation $mutation)
-    {
-        //
+        $data = array();
+        $data["id_siswa"] = $req->idS;
+        $data["id_kelas"] = $req->kelas;
+        $data["id_program_keahlian"] = $req->progKeah;
+        $data["tglMutasi"] = $req->tanggal;;
+        $data["noSuratMutasi"] = $req->noSurat;;
+        $data["pindah"] = $req->pindah;;
+        $data["AlasanPindah"] = $req->alasan;;
+        $data["suratPernyataan"] = "";
+        $data["berkasVideo"] = "";
+        $data["suratMutasi"] = "";
+        $tambah = DB::table("mutasi")->insert($data);
+        $datas = array();
+        $datas["status"] = "Tidak";
+        $update = DB::table("siswa")->where('id_siswa',$req->idS)->update($datas);
+        if ($tambah) {
+            return "S";
+        } else {
+            return "k";
+        }
     }
 }
--- resources/views/mutasi/add_mutasi.blade.php
+++ resources/views/mutasi/add_mutasi.blade.php
@@ -5,85 +5,86 @@
 
 @section('konten')
     <h1 class="h3 mb-3">Set Mutasi Siswa</h1>
+    <?php
+        $data = DB::table('siswa')->where('id_siswa', $id)->first();
+        
+        $progrmKeahlian = DB::table('program_keahlian')
+            ->where('status', "aktif")
+            ->get();
+            
+        $kelas = DB::table('kelas')
+            ->where('status', "aktif")
+            ->get();
+    ?>
     <div class="row">
         <div class="col-12">
-            <form>
+            <form id="simpan" data-parsley-validate method="post">
+            {{ csrf_field()}}
                 <div class="card">
                     <div class="card-header border-bottom py-1">
                         <h3 class="card-title mt-1">Mutasi Siswa</h3>
                     </div>
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-4">
-                                <label class="form-label" for="input1">Siswa</label>
-                                <input type="text" class="form-control mb-2 text-uppercase" id="input1" name=""
-                                    placeholder="siswa" required autocomplete="off">
-                            </div>
-                            <div class="col-md-4">
-                                <label class="form-label" for="input2">Prog. Keahlian</label>
-                                <select class="form-control mb-2" id="input2" name="" required>
-                                    <option value="">-Prog. Keahlian-</option>
-                                </select>
-                            </div>
-                            <div class="col-md-4">
-                                <label class="form-label" for="input3">Kelas</label>
-                                <select class="form-control mb-2" id="input3" name="" required>
-                                    <option value="">-Kelas-</option>
-                                </select>
-                            </div>
-                            <div class="col-md-4">
-                                <label class="form-label" for="input4">Jenis Mutasi</label>
-                                <select class="form-control mb-2" id="input4" name="" required>
-                                    <option value="">-Jenis Mutasi-</option>
-                                </select>
-                            </div>
-                            <div class="col-md-4">
-                                <label class="form-label" for="input5">No Surat Mutasi</label>
-                                <select class="form-control mb-2" id="input5" name="" required>
-                                    <option value="">-Jenis Mutasi-</option>
-                                </select>
-                            </div>
-                            <div class="col-md-4">
-                                <label class="form-label" for="input6">Dari/ke</label>
-                                <input type="text" class="form-control mb-2 text-uppercase" id="input6" name=""
-                                    placeholder="dari/ke" required autocomplete="off">
-                            </div>
-                            <div class="col-md-8">
-                                <label class="form-label" for="input7">Alasan</label>
-                                <textarea class="form-control mb-2 text-uppercase" id="input7" name="" rows="2" placeholder="Alasan"
-                                    required></textarea>
-                            </div>
-                            <div class="col-md-4">
-                                <label class="form-label" for="input8">Tanggal</label>
-                                <input type="date" class="form-control mb-2" id="input8" name="" placeholder=""
-                                    required autocomplete="off">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="card-header border-bottom py-1">
-                        <h3 class="card-title mt-1">Berkas Mutasi Siswa</h3>
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label class="form-label" for="input9">Surat Mutasi</label>
-                                <input type="file" class="form-control mb-2 text-uppercase" id="input9" name=""
-                                    required autocomplete="off">
-                            </div>
-                            <div class="col-md-4">
-                                <label class="form-label" for="input10">Bukti Video</label>
-                                <input type="file" class="form-control mb-2 text-uppercase" id="input10" name=""
-                                    required autocomplete="off">
-                            </div>
-                            <div class="col-md-4">
-                                <label class="form-label" for="input11">Surat Pernyataan</label>
-                                <input type="file" class="form-control mb-2 text-uppercase" id="input11" name=""
-                                    required autocomplete="off">
+                                <div class="col-md-4">
+                                    <label class="form-label" for="input1">Siswa</label>
+                                    <input type="text" class="form-control mb-2 text-uppercase"
+                                        placeholder="siswa" value="<?= $data->namaSiswa?>"  required readonly  >
+                                    <input type="hidden" value="<?= $data->id_siswa?>" name="idS"  required   >
+                                </div>
+                                <div class="col-md-4">
+                                    <label class="form-label" for="input4">alamat</label>
+                                    <input type="text" class="form-control mb-2 text-uppercase" id="input1" name=""
+                                        placeholder="siswa" value="<?= $data->detAlamat?>"  required readonly  >
+                                </div>
+                                <div class="col-md-4">
+                                <label class="form-label" for="input4">Program Keahlian</label>
+                                    <select class="form-control mb-3" id="input4" name="progKeah" required>
+                                        <option value="" hidden>Pilih Program Keahlian</option>
+                                        <?php
+                                            foreach ($progrmKeahlian as  $val) {
+                                                ?>
+                                                <option value="<?= $val->id_program_keahlian ?>"><?= $val->bidang_keahlian ?></option>
+                                            <?php
+                                                }
+                                            ?>
+                                    </select>
+                                </div>
+                                <div class="col-md-4">
+                                    <label class="form-label" for="input4">Kelas</label>
+                                    <select class="form-control mb-3" id="input4" name="kelas" required>
+                                        <option value="" hidden>Pilih Kelas</option>
+                                        <?php
+                                            foreach ($kelas as  $val) {
+                                                ?>
+                                                <option value="<?= $val->id_kelas ?>"><?= $val->kelas ?> . <?= $val->ruang?></option>
+                                            <?php
+                                                }
+                                            ?>
+                                    </select>
+                                </div>
+                                <div class="col-md-4">
+                                    <label class="form-label" for="input5">No Surat Mutasi</label>
+                                    <input type="text" class="form-control mb-2 text-uppercase" id="input6" name="noSurat"
+                                        placeholder="no surat"  required >
+                                </div>
+                                <div class="col-md-4">
+                                    <label class="form-label" for="input6">Pindah</label>
+                                    <input type="text" class="form-control mb-2 text-uppercase" id="input6" name="pindah"
+                                        placeholder="pindah" required >
+                                </div>
+                                <div class="col-md-8">
+                                    <label class="form-label" for="input7">Alasan</label>
+                                    <textarea class="form-control mb-2 text-uppercase" id="input7" name="alasan" rows="2" placeholder="Alasan"
+                                        required ></textarea>
+                                </div>
+                                <div class="col-md-4">
+                                    <label class="form-label" for="input8">Tanggal</label>
+                                    <input type="date" class="form-control mb-2" id="input8" name="tanggal" placeholder=""
+                                        required >
+                                </div> 
                             </div>
                         </div>
-                    </div>
                     <div class="card-footer">
                         <button type="button" class="btn btn-sm btn-danger">Batal</button>
                         <button class="btn btn-sm btn-primary float-end">Simpan</button>
@@ -92,4 +93,41 @@
             </form>
         </div>
     </div>
+    <script>
+        $("#simpan").on('submit', function(e) {
+                e.preventDefault()
+                var data = $(this).serialize()
+                var form = $(this)
+                form.parsley().validate()
+                if (form.parsley().isValid()) {
+                    $('#loading').css("display", "block")
+                    $.ajax({
+                        type: 'POST',
+                        url: '{{ route('mutasi.simpanMutasi') }}',
+                        data: data,
+                        success: function(hasil) {
+                            $('#loading').css("display", "none")
+                            if (hasil == 'k') {
+                                swal.fire({
+                                    title: 'Error',
+                                    text: 'Gagal dalam menyimpan data',
+                                    icon: 'error',
+                                    confirmButtonColor: '#3085d6'
+                                })
+                            } else if (hasil == "S") {
+                                swal.fire({
+                                    title: 'success',
+                                    text: 'Berhasil menyimpan data',
+                                    icon: 'success',
+                                    confirmButtonColor: '#3085d6',
+                                }).then(function () {
+                                    window.location.href = "{{ url('/mutation') }}" 
+                                    
+                                })
+                            }
+                        }
+                    })
+                }
+            })
+    </script>
 @endsection
